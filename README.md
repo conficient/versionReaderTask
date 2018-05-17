18531c58b120a2efe2c3af6aa3918f2b63672c41
Howard Richards <dev@conficient.com>
2018-05-17 16:22:48 +0100

    Inital version

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# versionReaderTask
+# VersionReaderTask
 VSTS build task to read Version tag from project files
+
+Reads the `<Version>` tag from new `csproj` and `vbproj` 2017 format files into environment variables.
+
+This tool was created to fix an issue with the new project format: it does not support wildcard in the version and therefore auto-append of build suffix.
+
+Optionally prefix the variable values so multiple projects can be read.
+
+
+Adapted from [AssemblyInfoReaderTask](https://github.com/kyleherzog/AssemblyInfoReaderTask) with thanks to [kyleherzog](https://github.com/kyleherzog)
---
+++ VersionReaderTask/VersionReaderTask.ps1
@@ -0,0 +1,40 @@
+﻿Param (
+    [string]$searchPattern = "**\*.??proj",
+	[string]$variablesPrefix
+)
+
+# Write all params to the console.
+Write-Host ("Search Pattern: " + $searchPattern)
+Write-Host ("Variables Prefix: " + $variablesPrefix)
+
+function SetBuildVariable([string]$varName, [string]$varValue)
+{
+	Write-Host ("Setting variable " + $variablesPrefix + $varName + " to '" + $varValue + "'")
+	Write-Output ("##vso[task.setvariable variable=" + $variablesPrefix + $varName + ";]" +  $varValue )
+}
+
+function SetVersionVariables([xml]$xml)
+{
+    $version = $xml.Project.PropertyGroup.Version
+	SetBuildVariable "Version", "$version"
+}
+
+$filesFound = Get-ChildItem -Path $searchPattern -Recurse
+
+if ($filesFound.Count -eq 0)
+{
+    Write-Warning ("No files matching pattern found.")
+}
+
+if ($filesFound.Count -gt 1)
+{
+   Write-Warning ("Multiple assemblyinfo files found.")
+}
+
+foreach ($fileFound in $filesFound)
+{
+    Write-Host ("Reading file: " + $fileFound)
+    #$fileText = [IO.File]::ReadAllText($fileFound)
+    [xml]$XmlDocument = Get-Content -Path $fileFound
+    SetVersionVariables($XmlDocument)
+}
