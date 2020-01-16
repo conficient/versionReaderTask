6b90e7467b42debafd99d7f4a726963b3e4e2014
Howard Richards <dev@conficient.com>
2020-01-16 10:42:36 +0000

    Amend task to show vars

--- VersionReaderTask/index.ts
+++ VersionReaderTask/index.ts
@@ -1,6 +1,13 @@
 /*
  *  VersionReaderV2 devops build task
  */
+var searchPattern = "**\*.??proj";
+var variablesPrefix = "";
+var buildPrefix = ".";
 
- console.log("VersionReader task V2");
+console.log("VersionReader task V2.0");
+console.log("=======================");
+console.log("Search Pattern: " + searchPattern)
+console.log("Variables Prefix: " + variablesPrefix)
+console.log("Build Prefix: " + buildPrefix)
  
